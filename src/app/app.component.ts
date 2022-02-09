df731a7ed7d35c66b2335302916ab66f4acef310
robin-john-menaa <robinprojectgcp@gmail.com>
2022-02-09 19:36:02 +0400

    REST API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    // console.log(postData);
+    this.http
+      .post(
+        "https://ng-complete-guide-ca764-default-rtdb.firebaseio.com/posts.json",
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  fetchPosts() {
+    this.http
+      .get(
+        "https://ng-complete-guide-ca764-default-rtdb.firebaseio.com/posts.json"
+      )
+      .subscribe((posts) => {
+        console.log(posts);
+      });
   }
 }
